09d6fd9c56121a47c67e020702d57d3b467d773b
カグチ <59391215+KagChi@users.noreply.github.com>
2021-06-02 10:58:55 +0700

    chore: rebase

--- src/structures/Node.ts
+++ src/structures/Node.ts
@@ -5,12 +5,12 @@ import Resolver from "./Resolver";
 export default class Node {
     public resolver = new Resolver(this);
 
-    public name!: string;
+    public id!: string;
     public host!: string;
     public port!: number | string;
     public auth!: string;
-    public secure?: string;
-    
+    public secure!: boolean;
+
     private readonly methods = {
         album: this.resolver.getAlbum.bind(this.resolver),
         playlist: this.resolver.getPlaylist.bind(this.resolver),
@@ -19,7 +19,7 @@ export default class Node {
 
     public constructor(public client: LavasfyClient, options: NodeOptions) {
         Object.defineProperties(this, {
-            name: { value: options.name, enumerable: true },
+            id: { value: options.id, enumerable: true },
             host: { value: options.host },
             port: { value: options.port },
             auth: { value: options.auth },
@@ -27,6 +27,10 @@ export default class Node {
         });
     }
 
+    /**
+     * A method for loading Spotify URLs
+     * @returns Lavalink-like /loadtracks response
+     */
     public load(url: string): Promise<LavalinkTrackResponse | null> {
         const [, type, id] = this.client.spotifyPattern.exec(url) ?? [];
         return this.methods[type as keyof Node["methods"]](id);
